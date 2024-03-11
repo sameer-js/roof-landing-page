182bd7e723a6590ea88fec8219a105e3fe5e5eba
Sameer <meersj@gmail.com>
2024-03-11 19:16:18 +0545

    added mock data

---
+++ src/utils/constants.js
@@ -0,0 +1,111 @@
+import {
+  BsGraphUpArrow,
+  BsHandThumbsUpFill,
+  BsCurrencyDollar,
+} from 'react-icons/bs';
+
+// SERVICES SECTION
+export const SERVICES = [
+  {
+    name: 'Roof Repairs',
+    price: '47,043',
+    detail:
+      'Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta',
+    image: './r1.png',
+  },
+  {
+    name: 'Roof Inspections',
+    price: '66,353',
+    detail:
+      'Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Barat',
+    image: './r2.png',
+  },
+  {
+    name: 'Roof Cleaning',
+    price: '35,853',
+    detail:
+      'Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten',
+    image: './r3.png',
+  },
+  {
+    name: 'Roof Coating',
+    price: '47,043',
+    detail:
+      'Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta',
+    image: './r1.png',
+  },
+  {
+    name: 'Roof Tiling',
+    price: '66,353',
+    detail:
+      'Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Barat',
+    image: './r2.png',
+  },
+  {
+    name: 'Roof Building',
+    price: '35,853',
+    detail:
+      'Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten',
+    image: './r3.png',
+  },
+];
+
+// VALUE SECTION
+
+export const VALUES = [
+  {
+    icon: <BsGraphUpArrow />,
+    heading: 'Best interest rates on the market',
+    detail:
+      'Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.',
+  },
+  {
+    icon: <BsHandThumbsUpFill />,
+    heading: 'Prevent unstable prices',
+    detail:
+      'Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.',
+  },
+  {
+    icon: <BsCurrencyDollar />,
+    heading: 'Best price on the market',
+    detail:
+      'Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.',
+  },
+];
+
+//   // FOOTER SECTION
+//   export const FOOTER_LINKS = [
+//     {
+//       title: 'Learn More',
+//       links: [
+//         'About Hilink',
+//         'Documentation',
+//         'Careers',
+//         'Privacy Policy',
+//         'Contact Us',
+//       ],
+//     },
+//     {
+//       title: 'Our Community',
+//       links: ['Climbing Nepal', 'Hiking Hills', 'Ghumante'],
+//     },
+//   ];
+
+//   export const FOOTER_CONTACT_INFO = {
+//     title: 'Contact Us',
+//     links: [
+//       { label: 'Admin Officer', value: '123-456-7890' },
+//       { label: 'Email Officer', value: 'hilink@akinthil.com' },
+//     ],
+//   };
+
+//   export const SOCIALS = {
+//     title: 'Socials',
+//     links: [
+//       '/facebook.svg',
+//       '/instagram.svg',
+//       '/twitter.svg',
+//       '/youtube.svg',
+//       '/wordpress.svg',
+//     ],
+//   };
